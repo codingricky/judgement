34ceecbe0887cf0313613372d1e965b97d149251
Ricky Yim <ricky@dius.com.au>
2017-08-15 22:22:42 +1000

    deleted unused classes

--- web/services/player_service.ex
+++ web/services/player_service.ex
@@ -4,19 +4,21 @@ defmodule Judgement.PlayerService do
     alias Judgement.Rating
     alias Judgement.Result
 
+    @k_factor 15
+
     def create_player(name, email) do
        result = %Player{name: name, email: email, rating: %Rating{}}
                           |> Repo.insert
        case result do
         {:ok, player} -> player
-        {:error, player} -> raise "problems creating a player" 
+        {:error, _player} -> raise "problems creating a player" 
        end
     end
 
     def create_result(winner, loser) do
       winner_rating_before = winner.rating.value
       loser_rating_before = loser.rating.value
-      {winner_rating_after, loser_rating_after} = Elo.rate(winner_rating_before, loser_rating_before, :win, k_factor: 15, round: :down)
+      {winner_rating_after, loser_rating_after} = Elo.rate(winner_rating_before, loser_rating_before, :win, k_factor: @k_factor, round: :down)
       %Result{winner: winner, 
               loser: loser,
               winner_rating_before: winner_rating_before,
--- web/views/user_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule Judgement.UserView do
-  use Judgement.Web, :view
-end
