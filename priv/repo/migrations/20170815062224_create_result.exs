5d8c8608f99d26ebad9ec175ee84a179183e1fa5
Ricky Yim <ricky@dius.com.au>
2017-08-15 22:55:06 +1000

    fixed migrations

--- priv/repo/migrations/20170815062224_create_result.exs
+++ priv/repo/migrations/20170815062224_create_result.exs
@@ -5,10 +5,11 @@ defmodule Judgement.Repo.Migrations.CreateResult do
     create table(:results) do
       add :winner_id, :integer
       add :loser_id, :integer
-      add :winner_points_before, :integer
-      add :winner_points_after, :integer
-      add :loser_points_before, :integer
-      add :loser_points_after, :integer
+
+      add :winner_rating_before, :integer
+      add :winner_rating_after, :integer
+      add :loser_rating_before, :integer
+      add :loser_rating_after, :integer
 
       timestamps()
     end
