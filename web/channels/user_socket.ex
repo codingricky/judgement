398fe5c0c78590644713adcb6009b3cb154c3822
Ricky Yim <ricky@dius.com.au>
2017-08-24 22:17:15 +1000

    deleted heroku stuff

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,8 +5,7 @@ defmodule Judgement.UserSocket do
   # channel "room:*", Judgement.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket,
-    timeout: 45_000
+  transport :websocket, Phoenix.Transports.WebSocket
   
   # transport :longpoll, Phoenix.Transports.LongPoll
 
