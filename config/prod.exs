4dbe150be76dcf20ccf1e7ea356464390e547f98
Ricky <ricky@dius.com.au>
2017-08-25 19:06:44 +1000

    more mods

--- config/prod.exs
+++ config/prod.exs
@@ -18,7 +18,7 @@ use Mix.Config
 #   cache_static_manifest: "priv/static/cache_manifest.json"
 
   config :judgement, Judgment.Endpoint,
-  http: [port: 4000],
+  http: [port: {:system, System.get_env("PORT")}, compress: true],
   url: [scheme: "http", host: System.get_env("HOST"), port: {:system, System.get_env("PORT")}],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   code_reloader: false,
