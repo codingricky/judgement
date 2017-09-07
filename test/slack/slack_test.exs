0fc91ae667408aaf47e686946c7b7e22a6e0bd6b
Ricky <ricky@dius.com.au>
2017-09-07 22:42:22 +1000

    moved code to setup

--- test/slack/slack_test.exs
+++ test/slack/slack_test.exs
@@ -9,7 +9,17 @@ defmodule Judgement.SlackTest do
     @channel "testing"
 
     setup do
-        :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+        Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+        
+        GameService.create_player("winner", "winner@example.com")
+        GameService.create_player("loser", "loser@example.com")
+        winner = GameService.find_player("winner@example.com")
+        loser = GameService.find_player("loser@example.com")        
+        # need 10 games for it to count
+        GameService.create_result(winner, loser, 5, false)
+        GameService.create_result(winner, loser, 5, false)
+
+        :ok
     end
 
     test "help" do
@@ -22,16 +32,7 @@ defmodule Judgement.SlackTest do
     end
 
     test "show" do
-        GameService.create_player("winner", "winner@example.com")
-        GameService.create_player("loser", "loser@example.com")
-        winner = GameService.find_player("winner@example.com")
-        loser = GameService.find_player("loser@example.com")        
-        # need 10 games for it to count
-        GameService.create_result(winner, loser, 5, false)
-        GameService.create_result(winner, loser, 5, false)
-
         expected_show = "1. *winner*  _10-0_ `1058 points` _100% 10_\n2. *loser*  _0-10_ `932 points` _0% 0_"
-        
         with_mock(MockSlackClient, [is_bot: fn(_user) -> false end,
                                 send_message: fn(channel, message, slack) -> IO.puts("#{inspect(channel)} #{inspect(message)} #{inspect(slack)}") end,
                                 post_message: fn(_channel, _message, _attachments) -> nil end]) do
